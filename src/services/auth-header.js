9d0c32a968f39e4214ee93cd67c40e45efde60f4
Cody C <cconroy11@gmail.com>
2020-09-28 01:20:17 -0400

    removed auth header and added in axios interceptors

--- src/services/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    var user = JSON.parse(localStorage.getItem('user'));
-  
-    if (user) {
-      return { 'Content-type': 'application/json', 'x-access-token': user.accessToken };
-    } else {
-      return { 'Content-type': 'application/json' };
-    }
-}
--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,18 @@
 import axios from "axios";
-import authHeader from './auth-header';
  
-export default axios.create({
+const http = axios.create({
   baseURL: "http://localhost:8080/api",
-  headers: authHeader()
 });
+
+http.interceptors.request.use((config) => {
+  var user = JSON.parse(localStorage.getItem('user'));
+  if(user){
+      config.headers.common = {'x-access-token': `${user.accessToken}`}
+  }
+  return config
+}, (err) => {
+  console.log(err)
+  return Promise.reject(err)
+})
+
+export default http;
